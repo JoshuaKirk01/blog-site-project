2809484ea2f61e8bc46fb9eacbd17fc36b21424d
Kirkster101 <83754901+Kirkster101@users.noreply.github.com>
2023-07-08 21:49:05 +1000

    Navbar and Page Templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TrendingComponent } from './trending/trending.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent, path: ''
+  },
+  {
+    component: TrendingComponent, path: 'trending'
+  },
+  {
+    component: FeedComponent, path: 'feed'
+  },
+  {
+    component: ProfileComponent, path: 'profile'
+  },
+  {
+    component: SignInComponent, path: 'sign-in'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TrendingComponent } from './trending/trending.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    TrendingComponent,
+    FeedComponent,
+    ProfileComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
